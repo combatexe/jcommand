906a65ce0d6014424d959d34b2dab725f4412d1c
fhoelting <frank@hoelting.de>
2016-11-20 20:04:14 +0100

    Add test environment for maven and Eclipse IDE

--- bundles/test/src/test/model/TestModel.java
+++ bundles/test/src/test/model/TestModel.java
@@ -1,5 +1,15 @@
 package test.model;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
 public class TestModel {
+
+	@Test
+	public void failTest() {
+		assertThat("Must fail", is(false));
+	}
 
 }
